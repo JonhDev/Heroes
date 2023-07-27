40fc1e76daa4f20dcf396e89d4bd5a65f1cf4cc6
Jonathan Bravo <johhdev01@gmail.com>
2023-07-27 10:21:43 -0600

    Update project

--- app/src/main/java/com/jonhbravo/heroes/core/network/NetworkResponse.kt
+++ app/src/main/java/com/jonhbravo/heroes/core/network/NetworkResponse.kt
@@ -1,12 +1,38 @@
 package com.jonhbravo.heroes.core.network
 
 import java.io.IOException
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 typealias HeroesNetworkResponse<S> = NetworkResponse<S, GenericNetworkError>
 
 sealed interface NetworkResponse<out T : Any, out U : Any> {
     data class Success<T : Any>(val body: T) : NetworkResponse<T, Nothing>
-    data class ApiError<U: Any>(val body: U, val code: Int) : NetworkResponse<Nothing, U>
+    data class ApiError<U : Any>(val body: U, val code: Int) : NetworkResponse<Nothing, U>
     data class NetworkError(val error: IOException) : NetworkResponse<Nothing, Nothing>
     data class UnknownError(val error: Throwable) : NetworkResponse<Nothing, Nothing>
+}
+
+@OptIn(ExperimentalContracts::class)
+inline fun <T : Any> HeroesNetworkResponse<T>.onSuccess(action: (result: T) -> Unit): HeroesNetworkResponse<T> {
+    contract {
+        callsInPlace(action, InvocationKind.AT_MOST_ONCE)
+    }
+    if (this is NetworkResponse.Success) action(this.body)
+    return this
+}
+
+inline fun <T : Any> HeroesNetworkResponse<T>.onApiError(action: (result: GenericNetworkError, code: Int) -> Unit): HeroesNetworkResponse<T> {
+    if (this is NetworkResponse.ApiError) action(this.body, this.code)
+    return this
+}
+
+inline fun <T : Any> HeroesNetworkResponse<T>.onError(action: (exception: Throwable) -> Unit): HeroesNetworkResponse<T> {
+    when(this) {
+        is NetworkResponse.NetworkError -> this.error
+        is NetworkResponse.UnknownError -> this.error
+        else -> null
+    }?.let(action)
+    return this
 }
--- app/src/main/java/com/jonhbravo/heroes/domain/usecases/GetHeroesUseCase.kt
+++ app/src/main/java/com/jonhbravo/heroes/domain/usecases/GetHeroesUseCase.kt
@@ -2,6 +2,8 @@ package com.jonhbravo.heroes.domain.usecases
 
 import com.jonhbravo.heroes.core.di.IODispatcher
 import com.jonhbravo.heroes.core.network.NetworkResponse
+import com.jonhbravo.heroes.core.network.onApiError
+import com.jonhbravo.heroes.core.network.onSuccess
 import com.jonhbravo.heroes.core.utils.HeroesException
 import com.jonhbravo.heroes.core.utils.Mapper
 import com.jonhbravo.heroes.data.model.HeroesApiModel
@@ -18,9 +20,13 @@ class GetHeroesUseCase @Inject constructor(
 ) {
 
     suspend operator fun invoke(): List<HeroUiModel> = withContext(ioDispatcher) {
-        val response = heroesRepository.getHeroes()
-        if (response is NetworkResponse.Success<HeroesApiModel>) {
-            heroesMapper.map(response.body)
-        } else throw HeroesException("Error getting heroes")
+        heroesRepository.getHeroes()
+            .onSuccess { response ->
+                return@withContext heroesMapper.map(response)
+            }
+            .onApiError { result, code ->
+
+            }
+        throw HeroesException("")
     }
 }
